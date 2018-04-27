ce92d1c2dbd8748cec29c02135fbe00f8dc63da6
Tariod <wasd1299@gmail.com>
2018-04-27 21:46:52 +0300

    Implementation of the problem-13 resolving.

--- problem-13/problem-13.cpp
+++ problem-13/problem-13.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
+#include <string>
 #include <ctime>
 #include "../sort-agorithm/sortAlgorithm.h"
+using namespace std;
 
 int main() {
     srand(time(NULL));
-	int size = 15;
-	int *array = new int[size];
-    for(int i = 0; i < size; i++)
+    string value;
+    int size;
+    int *array;
+
+    cout << "Enter the size of the array:" << endl;
+	getline(cin, value);
+    if(!value.empty())
+	    size = stoi(value);
+	else
+        return 0;
+
+    array = new int[size];
+    for(int i = 0; i < size; i++) {
         array[i] = -99 + rand() % 199;
-    for(int i = 0; i < size; i++)
-        std::cout << array[i] << " ";
+        cout << array[i] << " ";
+    }
     std::cout << std::endl;
-    sortAlgorithm test = sortAlgorithm(array, size);
-    int *arr = test.ShellSort();
+
+    sortAlgorithm *test = new sortAlgorithm(array, size);
+
+    int *ShellSort = test->ShellSort();
+    cout << "ShellSort:" << endl;
     for(int i = 0; i < size; i++)
-        std::cout << arr[i] << " ";
+        std::cout << ShellSort[i] << " ";
     std::cout << std::endl;
+
+    int *QuickSort = test->QuickSort(0, size - 1);
+    cout << "QuickSort:" << endl;
+    for(int i = 0; i < size; i++)
+        std::cout << QuickSort[i] << " ";
+    std::cout << std::endl;
+
+    delete test;
+    delete []QuickSort;
+    delete []ShellSort;
+
+    return 0;
 }
